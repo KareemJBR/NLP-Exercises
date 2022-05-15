1d7863aebeb2da3b666f1db9ca087afdd82255ea
Kareem <77075186+KareemJBR@users.noreply.github.com>
2022-05-15 13:06:32 +0300

    Added ex5 files

---
+++ HW5/jabareen_kareem_ex5.py
@@ -0,0 +1,19 @@
+from sys import argv
+
+
+# Implement your code here #
+
+
+if __name__ == '__main__':
+
+    input_grammar = argv[1]         # The name of the file that contains the probabilistic grammar
+    input_sentences = argv[2]       # The name of the file that contains the input sentences (tests)
+    output_trees = argv[3]          # The name of the output file
+
+    # Implement your code here #
+
+
+
+
+
+
